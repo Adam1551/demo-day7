47c3af8a98a12160c838302181f43ad98309cbee
Adam1551 <adam1.johannesson@gmail.com>
2022-09-15 11:10:19 +0200

    if satser

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hur många studerande är vi idag? ");
+        int numberOfStudents = scanner.nextInt();
+        boolean goodMood = true;
+
+        if (numberOfStudents == 33) {
+            System.out.println("Alla verkar vara här idag");
+            if (goodMood) {
+                System.out.println("Alla verkar dessutom vara glada idag");
+            }
+        }
+
+        if (numberOfStudents > 33) {
+            System.out.println("Någon i klassrummet verkar ha hamnat fel");
+        }
+
+        if (numberOfStudents < 33) {
+            System.out.println("Det fattas några elever idag");
+        }
     }
 }
